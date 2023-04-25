29efda73b60abf37cfdfdb48a100e27edbff70a9
Анастасия Ефремова <adam.dmitrienko@icloud.com>
2023-04-25 10:30:33 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace Cesar_code
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void encryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textToEncrypt = textToEncryptTextBox.Text;
+            int encryptionKey;
+            if (int.TryParse(encryptionKeyTextBox.Text, out encryptionKey))
+            {
+                string encryptedText = Encrypt(textToEncrypt, encryptionKey);
+                encryptedTextBox.Text = encryptedText;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid encryption key (0-25).");
+            }
+        }
+
+        private string Encrypt(string textToEncrypt, int encryptionKey)
+        {
+            string encryptedText = "";
+            const int alphabetSize = 26;
+
+            foreach (char c in textToEncrypt)
+            {
+                if (char.IsLetter(c))
+                {
+                    char currentChar = char.ToLower(c);
+                    int shiftedChar = currentChar + encryptionKey;
+
+                    if (shiftedChar > 'z')
+                    {
+                        shiftedChar -= alphabetSize;
+                    }
+
+                    encryptedText += char.IsLower(c) ? (char)shiftedChar : char.ToUpper((char)shiftedChar);
+                }
+                else
+                {
+                    encryptedText += c;
+                }
+            }
+
+            return encryptedText;
+        }
+    }
+}
